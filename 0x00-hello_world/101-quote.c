3843256899296d607b773277f92435407cef40ee
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-08 18:32:42 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,7 @@
+#include <unistd.h>
+int main ( )
+	/* C program that prints a word exactly without using printf or puts */
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return 1;
+}
