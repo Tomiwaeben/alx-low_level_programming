f61ca69e9c8a10821f7a60e84ad43e4b54e9eff3
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-11 10:10:53 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always(Success)
+ */
+int main()
+{
+	int x, y, z;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		
+		for (y = '1'; y <= '9'; y++)
+		{
+			
+			for (z = '2'; z <= '9'; z++)
+			{
+			if (z > y && y > x)
+			{
+
+				putchar(x + '0');
+			        putchar(y + '0');
+		                putchar(z + '0');
+			        	if (i != '7')
+
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			
+			}
+		}
+	}
+	
+	putchar('\n');
+	return 0
+}
