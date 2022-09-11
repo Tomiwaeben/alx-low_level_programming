21de0f23d0d089ac53344f1d25ee94312e295c47
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-10 17:10:59 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main: prints single digits of base 10
+ * return: 0
+ */
+int main()
+{
+	char n;
+	for(n = '0'; n <= '9'; n++);
+	{
+		putchar( n);
+	}
+	putchar( '\n');
+	return 0
+}
