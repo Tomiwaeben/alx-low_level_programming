db3428d6c974180b802081a0c96436033c9e59c6
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-11 09:38:57 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - prints all possible different combinations of two digits
+ * Return: Always (Success)
+ */
+int main()
+{
+	int x,y;
+
+	for (x = '0', x <= '9', x++)
+	{
+		for (y = '1', y <= '9', y++)
+		{
+			if (y > x)
+			{
+				putchar(x + '0');
+				putchar(y + '0');
+
+				if (x != '8')
+				{
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return 0;
+}
