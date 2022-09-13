890e4291404a004aca07d89ac9ebfc48a6ed2d5c
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-13 16:00:02 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,10 +5,17 @@
  *
  * Return: Always 0.
  */
+
 int main()
 {
-	char ch = '_putchar';
-	_putchar(ch);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return 0;
