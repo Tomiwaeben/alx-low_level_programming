a319c4495202ec99f21f24e0838cb07a7538b575
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-14 03:43:12 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,22 +20,22 @@ void times_table(void)
 			{
 				u = z % 10;
 				d = (z - u) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
+				putchar(44);
+				putchar(32);
+				putchar(d + '0');
+				putchar(u + '0');
 			}
 			else
 			{
 				if (y != 0)
 				{
-					_pitchar(44);
-					_putchar(32);
-					_putchar(32);
+					pitchar(44);
+					putchar(32);
+					putchar(32);
 				}
-				_putchar(z + '0');
+				putchar(z + '0');
 			}
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
