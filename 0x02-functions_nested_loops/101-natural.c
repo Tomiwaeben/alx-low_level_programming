8351cf18137fe23e88e3f8e3919078141126df98
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-14 15:39:34 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all  multiples of 3 & 5 below 1024
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	int i;
+	int z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+
+		i++;
+	}
+
+	printf("%d\n", z);
+	return (0);
+}
