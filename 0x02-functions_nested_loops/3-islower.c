b07dc97b38db46c19076084fe87bb32b3dd6e8f9
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-13 17:18:56 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks if an alphabet is lowercase
+ * Return 1 if c is lowercase
+ * Return 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z')
+}
