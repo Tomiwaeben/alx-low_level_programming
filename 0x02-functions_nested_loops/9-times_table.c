3a55357c034c4cba2771f9394a2c1b68cee4e443
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-14 04:00:29 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,27 +15,27 @@ void times_table(void)
 	{
 		for (y = 0; y <= 9; y++)
 		{
-			z = x * y;
+			z = (x * y);
 			if (z > 9)
 			{
-				u = z % 10;
-				d = (z - u) / 10;
-				putchar('44');
-				putchar('32');
-				putchar(d + '0');
-				putchar(u + '0');
+				u = (z % 10);
+				d = ((z - u) / 10);
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
 			else
 			{
 				if (y != 0)
 				{
-					putchar('44');
-					putchar('32');
-					putchar('32');
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
 				}
-				putchar(z + '0');
+				_putchar(z + '0');
 			}
 		}
-		putchar('\n');
+		_putchar('\n');
 	}
 }
