846c703d7bb3b49e20ca4b57d55f02537a5acf6f
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-15 21:30:26 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	for (c = 48; c = 58; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
