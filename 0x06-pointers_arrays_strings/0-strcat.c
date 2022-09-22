08d66e686cd4a0dd0f69b87e43535cc24b420fa1
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-22 14:11:22 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,12 +8,14 @@
  * Return: two concat strings
  */ 
  char *_strcat(char *dest, char *src)
-{ 
+{
+	
 	int i, n;
 	
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
+	
 	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
 	{
 	}
