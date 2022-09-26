2104763365a9af0e465aedd0a108280d60ebcab6
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-26 16:10:50 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string to be searched
+ * @c: character to be located
+ *
+ * Return: if c is found - pointer to c
+ * if not - NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+	
+	return ('\0');
+}
