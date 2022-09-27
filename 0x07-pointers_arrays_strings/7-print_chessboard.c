b7b3f1e20b99b7074a2ef1d1fd5dc3e713704317
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-26 17:08:32 -0700

    7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard
+ * @a: The chessboard to be printed
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; a[x][7]; x++)
+	{
+		for (y = 0; y < 8; y++)
+			_putchar(a[x][y]);
+
+		_putchar('\n')
+	}
+}
