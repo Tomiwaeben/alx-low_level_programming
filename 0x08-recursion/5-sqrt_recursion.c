812471902d8496b125dd5bc86190b31d78285e21
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-28 20:34:49 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,11 +10,11 @@
  */
 int sqrt2(int a, int b)
 {
-	if (b * b == a)
-		return (b)
-		else if (b * b > a)
-		return (-1)
-		return (sqrt2(a, b + 1));
+	if (b * b == a)
+		return (b)
+		else if (b * b > a)
+		return (-1)
+		return (sqrt2(a, b + 1));
 }
 /**
  * _sqrt_recursion - returns the natural square root of n
@@ -22,7 +22,7 @@ int sqrt2(int a, int b)
  * Return: On success 1
  * On error, -1 is returned, and error is set appropriately
  */
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n)
 {
-	return (sqrt2(n, 1));
+	return (sqrt2(n, 1));
 }
