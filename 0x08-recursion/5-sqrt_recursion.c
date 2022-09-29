e1d89ad7155b9b7003bba75b06f834da8a3b0387
Tomiwa Akande <akandetomiwaeben@gmail.com>
2022-09-28 20:45:31 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,9 +11,9 @@
 int sqrt2(int a, int b)
 {
 	if (b * b == a)
-		return (b)
-		else if (b * b > a)
-		return (-1)
+		return (b);
+		else if (b * b > a)
+		return (-1);
 		return (sqrt2(a, b + 1));
 }
 /**
@@ -24,5 +24,5 @@ int sqrt2(int a, int b)
  */
 int _sqrt_recursion(int n)
 {
-	return (sqrt2(n, 1));
+	return (sqrt2(n, 1));
 }
